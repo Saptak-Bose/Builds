66fb6f7f22ac4423826a538d0c53925d2e5750af
Saptak <subratabose11@gmail.com>
2022-02-23 11:43:36 +0530

    commit...unknown

---
+++ spotify-clone2.0/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCl9KgidM8H7YkPNuV4hNaMZVYeb2WtDkk",
+    authDomain: "spotify-2-2e4c0.firebaseapp.com",
+    projectId: "spotify-2-2e4c0",
+    storageBucket: "spotify-2-2e4c0.appspot.com",
+    messagingSenderId: "738112125675",
+    appId: "1:738112125675:web:fd9eb58e799b860c9fdcae",
+    measurementId: "G-Z7SP2D8WYC"
+};
