fdc1caa1c7c755f8d943a25761c528ad94c50609
Saptak <subratabose11@gmail.com>
2022-03-01 21:04:33 +0530

    commit

--- facebook-sap/next.config.js
+++ facebook-sap/next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
---
+++ facebook-sap/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
