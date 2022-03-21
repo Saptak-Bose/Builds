21e511a3623b74b09160c00cfe1f5789cbff3678
Saptak <subratabose11@gmail.com>
2022-03-21 22:22:32 +0530

    commit

---
+++ what-2/components/Message.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+function Message() {
+  return <Container></Container>;
+}
+
+export default Message;
+
+const Container = styled.div``;
--- what-2/pages/chat/[id].js
+++ what-2/pages/chat/[id].js
@@ -1,18 +1,22 @@
 import Head from "next/head";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
 import ChatScreen from "../../components/ChatScreen";
 import Sidebar from "../../components/Sidebar";
-import { db } from "../../firebase";
+import { auth, db } from "../../firebase";
+import getRecipientEmail from "../../utils/getRecipientEmail";
 
 function Chat({ chat, messages }) {
+  const [user] = useAuthState(auth);
+
   return (
     <Container>
       <Head>
-        <title>Chat</title>
+        <title>Chat with {getRecipientEmail(chat.users, user)}</title>
       </Head>
       <Sidebar />
       <ChatContainer>
-        <ChatScreen />
+        <ChatScreen chat={chat} messages={messages}/>
       </ChatContainer>
     </Container>
   );
