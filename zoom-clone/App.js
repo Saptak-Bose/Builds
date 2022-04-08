9b85112ffd3aa5a9b3f612f7347ee90d313759b6
Saptak <subratabose11@gmail.com>
2022-04-08 19:51:59 +0530

    commit

--- zoom-clone/App.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import Home from "./screens/Home";
-import Navigation from "./Navigation";
-
-export default function App() {
-  return <Navigation />
-}
