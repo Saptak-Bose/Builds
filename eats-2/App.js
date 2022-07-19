1691083072fb0a709e86580bab4cfe158273a3b1
Saptak <subratabose11@gmail.com>
2022-07-19 17:38:02 +0530

    c

--- eats-2/App.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Home from "./screens/Home";
-
-export default function App() {
-  return (
-    <Home />
-  );
-}
