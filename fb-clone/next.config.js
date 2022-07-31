7913ac291be40a271b87b1fd580c8ce4334bd2f2
Saptak <subratabose11@gmail.com>
2022-07-31 18:56:50 +0530

    c

--- fb-clone/next.config.js
+++ fb-clone/next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+      "avatars.githubusercontent.com",
+    ],
+  },
+};
