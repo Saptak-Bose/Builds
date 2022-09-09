4c8020756bde954c023766bdd56c967fa08f80d6
Saptak <subratabose11@gmail.com>
2022-09-09 21:15:59 +0530

    s

---
+++ Java/Learn Java/decimal.java
@@ -0,0 +1,11 @@
+public class decimal
+{
+    public static void main(double a, double b, double c)
+    {
+        double s, avg;
+        s = (double) a + b + c;
+        avg = (double) s/3;
+        System.out.println("Sum = "+s);
+        System.out.println("Average = "+avg);
+    }
+}
