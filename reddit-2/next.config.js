3b2d1211d9e9e8ab8b077b72b4468abb1f2d0cc7
Saptak <subratabose11@gmail.com>
2022-09-13 18:01:40 +0530

    h

--- reddit-2/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["links.papareact.com", "commons.wikimedia.org", "upload.wikimedia.org"],
-  },
-};
