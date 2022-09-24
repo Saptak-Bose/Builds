bbe142f830bb543cfb7bbf32077d18c3ea49e0a0
Saptak <subratabose11@gmail.com>
2022-09-24 18:21:27 +0530

    Uber-3 cannot be carried out further more for technical issues. This will be soon deleted from the repo

---
+++ uber-3/screens/MapScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function MapScreen() {
+  return (
+    <View>
+      <Text>Here is the map stuff...</Text>
+    </View>
+  );
+}
