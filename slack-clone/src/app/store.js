de76666aaecf4c009f3479de9ef3e7927650c166
Saptak <subratabose11@gmail.com>
2022-11-19 10:52:47 +0530

    d

--- slack-clone/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import appReducer from '../features/appSlice';
-
-export const store = configureStore({
-  reducer: {
-    app: appReducer,
-  },
-});
