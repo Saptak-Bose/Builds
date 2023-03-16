7a22d0ca39f79c8a5f3a585efdbc258131e1a329
Saptak Bose <ranicob2014@gmail.com>
2023-03-16 19:05:33 +0530

    c

--- insta-clone/components/Modal.js
+++ insta-clone/components/Modal.js
@@ -151,6 +151,7 @@ const Modal = () => {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
+                    disabled={!selectedFile}
                     className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
                     onClick={uploadPost}
                   >
